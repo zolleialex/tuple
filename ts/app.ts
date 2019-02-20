cafb6c94709c176f605c2ca3200f16e790be4a0f
zolleialex <47735992+zolleialex@users.noreply.github.com>
2019-02-20 11:36:03 +0100

    Add files via upload

---
+++ ts/app.ts
@@ -0,0 +1,10 @@
+﻿import * as http from "http";
+import Content from "./content";
+
+class Program {
+    constructor() {
+        http.createServer(new Content().content).listen(process.env.PORT || 8080);
+    }
+}
+
+new Program();
---
+++ ts/content.ts
@@ -0,0 +1,22 @@
+﻿import * as http from "http";
+
+export default class Content {
+
+    public content(req: http.IncomingMessage, res: http.ServerResponse): void {
+        res.write("<a href='https://github.com/zolleialex/tuple' target='_blank'>" +
+            "https://github.com/zolleialex/tuple</a><br>");
+        let tupleType: [string, number];
+
+        tupleType = ['hey', 13];
+        // tupleType = [5, 'hey']; // Error - '[number, string]' is not assignable to '[string, number]'
+        // tupleType = ['hey', 'tuple']; // Error - '[string, string]' is not assignable to '[string, number]'
+
+        tupleType[3] = 54; // this works well because union type string | number is used for indices outside of known ones
+        tupleType[4] = 'test'; // works well because it can be string or number, union type
+        // tupleType[5] = true; // Error - Type 'true' is not assignable to type 'string | number'
+
+        console.log(tupleType);
+
+        res.end();
+    }
+}
