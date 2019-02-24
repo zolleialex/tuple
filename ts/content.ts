177fcdd7652b63ec37e2a4b97f483f24341770a6
zolleialex <47735992+zolleialex@users.noreply.github.com>
2019-02-24 21:11:09 +0100

    Add files via upload

--- ts/content.ts
+++ ts/content.ts
@@ -5,29 +5,24 @@ export default class Content {
 
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 		let mytuple = [10, "Hello", "World", "typeScript"]; 
-		res.write("<p> Items before push " + mytuple.length+"</p>");   // visszaadja a tuple méretét 
+		res.write("<h1> Items before push " + mytuple.length+"</h1>");   // visszaadja a tuple méretét 
 
 		mytuple.push(12);                                   // hozzáad egy értéket a tuplehoz
-		res.write("<p> Items after push " + mytuple.length+"</p>");
-		res.write("<p> Items before pop " + mytuple.length+"</p>");
-		res.write("<p>"+"mytuple.pop() + " popped from the tuple </p>"); // kiszedi és visszadja az utolsó elemet
-		res.write("<p> Items after pop " + mytuple.length+"</p>");
+		res.write("<h1> Items after push " + mytuple.length+"</h1>");
+		res.write("<h1> Items before pop " + mytuple.length+"</h1>");
+		res.write("<h1>"+"mytuple.pop() + " popped from the tuple +"</h1>"); // kiszedi és visszadja az utolsó elemet
+		res.write("<h1> Items after pop " + mytuple.length+"</h1>");
 		// Tuplik frissitése
 		let mytuple2 = [10, "Hello", "World", "typeScript"]; // tuple létrehozása
-		res.write("<p> Tuple value at index 0 " + mytuple[0]+"</p>");
+		res.write("<h1> Tuple value at index 0 " + mytuple[0]+"</h1>");
 
 		// tuple 0. elemének megváltoztatása
 		mytuple[0] = 121;
-		res.write("<p> Tuple value at index 0 changed to   " + mytuple[0]+"</p>");
+		res.write("<h1> Tuple value at index 0 changed to   " + mytuple[0]+"</h1>");
 		// Üres tuple
 		let tup = [] ;
 		tup[0] = 12 ;
 		tup[1] = 23 ;
-
-		res.write("<p>"+tup[0]+"</p>") ;
-		res.write("<p>"+tup[1]+"</p>");
-
-        
         res.write("<a href='https://github.com/zolleialex/tuple' target='_blank'>" +
             "https://github.com/zolleialex/tuple</a><br>");
         res.end();
