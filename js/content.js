6096224223335e8f2821c72f4410604b1577f4be
zolleialex <47735992+zolleialex@users.noreply.github.com>
2019-02-24 22:03:52 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,21 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Content {
     content(req, res) {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-	var mytuple: [number, string, string, string] = [10, "Hello", "World", "typeScript"];
+	let mytuple = [10, "Hello", "World", "typeScript"]; 
 	res.write("<p>Tuple elemei:</p>");
 	var i;
 	for (i = 0; i < mytuple.length; i++) { 
   		res.write(mytuple[i]+",");
 	}
+	res.write("<p>Elemek száma " + mytuple.length+"</p>");
 	mytuple.push(12); // hozzáad egy értéket a tuplehoz
-	res.write("<p>Elemek száma: " + mytuple.length+"</p>");
 	res.write("<p>Hozzáadunk egy elemet: </p>");
 	var y;
 	for (y = 0; y < mytuple.length; y++) { 
   		res.write(mytuple[i]+",");
 	}	    
 	res.write("<p>"+mytuple.pop()+" kitörölve a tupleból</p>"); // kiszedi és visszadja az utolsó elemet
-	res.write("<p>Elemek száma: " + mytuple.length+"</p>");
+	res.write("<p>Elemek száma " + mytuple.length+"</p>");
 	// Tuplik frissitése
 	res.write("<p>Tuple 0. elemének az értéke: " + mytuple[0]+"</p>");
 	// tuple 0. elemének megváltoztatása
