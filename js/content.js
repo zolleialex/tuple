7d55f32750223e7ee5817a08d333427d1f3051f7
zolleialex <47735992+zolleialex@users.noreply.github.com>
2019-02-24 21:36:00 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -2,7 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class Content {
     content(req, res) {
-        
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+		let mytuple = [10, "Hello", "World", "typeScript"]; 
+		 res.write("<p>Items before push: "+ mytuple.length+"</p>");   // visszaadja a tuple méretét 
+
+		mytuple.push(12);                                   // hozzáad egy értéket a tuplehoz
+		res.write("<p>Items after push: " + mytuple.length+"</p>");
+		res.write("<p>Items before pop " + mytuple.length+"</p>");
+		res.write("<p>"+mytuple.pop()+" popped from the tuple </p>"); // kiszedi és visszadja az utolsó elemet
+		res.write("<p> Items after pop: " + mytuple.length+"</p>");
+		// Tuplik frissitése
+		let mytuple2 = [10, "Hello", "World", "typeScript"]; // tuple létrehozása
+		res.write("<p>Tuple value at index 0: " + mytuple[0]+"</p>");
+
+		// tuple 0. elemének megváltoztatása
+		mytuple[0] = 121;
+		res.write("<p>Tuple value at index 0 changed to: " + mytuple[0]+"</p>");
         res.write("<a href='https://github.com/zolleialex/tuple' target='_blank'>" +
             "https://github.com/zolleialex/tuple</a><br>");
         res.end();
