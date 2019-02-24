a5e617ce98ed383bcb2e32afc003c420179d0b2c
zolleialex <47735992+zolleialex@users.noreply.github.com>
2019-02-24 20:57:50 +0100

    Add files via upload

--- ts/content.ts
+++ ts/content.ts
@@ -1,22 +1,35 @@
-﻿let mytuple = [10, "Hello", "World", "typeScript"]; 
-console.log("Items before push " + mytuple.length);   // visszaadja a tuple méretét 
+﻿
+export default class Content {
 
-mytuple.push(12);                                   // hozzáad egy értéket a tuplehoz
-console.log("Items after push " + mytuple.length);
-console.log("Items before pop " + mytuple.length);
-console.log(mytuple.pop() + " popped from the tuple"); // kiszedi és visszadja az utolsó elemet
-console.log("Items after pop " + mytuple.length);
-// Tuplik frissitése
-let mytuple2 = [10, "Hello", "World", "typeScript"]; // tuple létrehozása
-console.log("Tuple value at index 0 " + mytuple[0]);
+    public content(req: http.IncomingMessage, res: http.ServerResponse): void {
 
-// tuple 0. elemének megváltoztatása
-mytuple[0] = 121;
-console.log("Tuple value at index 0 changed to   " + mytuple[0]);
-// Üres tuple
-let tup = [] ;
-tup[0] = 12 ;
-tup[1] = 23 ;
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+		let mytuple = [10, "Hello", "World", "typeScript"]; 
+		res.write("Items before push " + mytuple.length);   // visszaadja a tuple méretét 
 
-console.log(tup[0]) ;
-console.log(tup[1]);
+		mytuple.push(12);                                   // hozzáad egy értéket a tuplehoz
+		res.write("Items after push " + mytuple.length);
+		res.write("Items before pop " + mytuple.length);
+		res.write(mytuple.pop() + " popped from the tuple"); // kiszedi és visszadja az utolsó elemet
+		res.write("Items after pop " + mytuple.length);
+		// Tuplik frissitése
+		let mytuple2 = [10, "Hello", "World", "typeScript"]; // tuple létrehozása
+		res.write("Tuple value at index 0 " + mytuple[0]);
+
+		// tuple 0. elemének megváltoztatása
+		mytuple[0] = 121;
+		res.write("Tuple value at index 0 changed to   " + mytuple[0]);
+		// Üres tuple
+		let tup = [] ;
+		tup[0] = 12 ;
+		tup[1] = 23 ;
+
+		res.write(tup[0]) ;
+		res.write(tup[1]);
+
+        
+        res.write("<a href='https://github.com/zolleialex/tuple' target='_blank'>" +
+            "https://github.com/zolleialex/tuple</a><br>");
+        res.end();
+    }
+}
